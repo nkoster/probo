9c75dc0621dfc9fada918aab250bff0f65d34c62
Niels Koster <n.koster@portavita.eu>
2017-07-30 23:50:01 +0200

    Chain

--- make.bash
+++ make.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 gp='g++ -O -Wall -std=c++11'
-$gp hi.cpp -o hi
-$gp v.cpp -o v_tmp && cat v_tmp hi >v && chmod +x v
 
-cp /bin/ls .
-cp hi newhi
-cp hi other
-
+$gp hi.cpp -o hi && \
+$gp v.cpp -o v_tmp && cat v_tmp hi >v && chmod +x v && \
+cp /bin/ls . && \
+cp hi newhi && \
+cp hi other && \
 ls -l
